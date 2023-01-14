d6578e56b033915310d61286fc637623bee8723a
Vaishnavi <vnb0902@outlook.com>
2023-01-14 15:39:44 +0530

    added new attributes

--- repo_intro/hello.java
+++ repo_intro/hello.java
@@ -1,7 +1,10 @@
 //comment
 public class hello{
     public static void main(String[] args){
-        int x = 10;
+        int x = 6;
+        int y = 8;
         System.out.println(x++);
+        System.out.println(x+y);
+
     }
 }
